ae233bd7ba26cd636bcefbeecbbc74eca85ebbda
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-09-18 13:00:21 +0530

    template inheritance

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request, 'shop/index.html')
 
 def about(request):
-    return HttpResponse("About")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("Contact")
