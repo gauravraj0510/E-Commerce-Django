323bc565355fd4417b58016d61ad6079f41d3ad0
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-09-19 18:43:05 +0530

    contact form completed

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from .models import Contact
 from math import ceil
 # Create your views here.
 
@@ -21,7 +22,15 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return render(request, 'shop/contact.html')
+    if request.method=="POST":
+        print(request)
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, "shop/contact.html")
 
 def tracker(request):
     return render(request, 'shop/tracker.html')
