85f7abdc591bcaf52fa8bd1213edf780081ef7e8
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-09-19 17:49:41 +0530

    product view completed

--- shop/views.py
+++ shop/views.py
@@ -29,8 +29,10 @@ def tracker(request):
 def search(request):
     return render(request, 'shop/search.html')
 
-def productView(request):
-    return render(request, 'shop/prodView.html')
+def products(request, myid):
+    #Fetch product using ID
+    product = Product.objects.filter(id = myid)
+    return render(request, 'shop/prodView.html',{'product':product[0]})
 
 def checkout(request):
     return render(request, 'shop/checkout.html')
